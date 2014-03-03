34ce5f45f8e1880b9d204bc79620bc946689d278
bluecerebudgerigar <bluecerebudgerigar@gmail.com>
2014-03-03 17:32:30 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='lncrnadb_table',
     version='0.1',
-    packages=['lncrnadb_table'],
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License, I think',  # example license
     description='djangocms with added features for lncrnadb usage',
