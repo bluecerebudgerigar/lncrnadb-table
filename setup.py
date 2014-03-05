837503c4182d37af05e23f4e06488ada492ed12b
bluecerebudgerigar <bluecerebudgerigar@gmail.com>
2014-03-05 17:18:16 +1100

    test

--- setup.py
+++ setup.py
@@ -7,29 +7,16 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='lncrnadb-table',
+    name='lncrnadbtable',
     version='0.1',
-    packages=['lncrnadb-table'],
-    provides=['lncrnadb-table'],
+    packages=['lncrnadbtable'],
+    provides=['lncrnadbtable'],
     include_package_data=True,
     license='BSD License, I think',  # example license
     description='djangocms with added features for lncrnadb usage',
     long_description=README,
-    url='https://github.com/bluecerebudgerigar/lncrnadb_table',
+    url='https://github.com/bluecerebudgerigar/lncrnadb-table',
     author='Quek Xiu Cheng',
     author_email='quekxiucheng@gmail.com',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: DUCK Approved :: BSD License', # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
 )
 
